cff9b6e4df91cd9d742631c3117b76f0055927ee
Emma Craig <emma.craig@getflowbox.com>
2024-03-13 17:17:29 +0100

    update links

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 // access the DOM
 
-const boxes = document.querySelectorAll('.box')
-const text = document.querySelector('#heading')
-const strategy = document.querySelector('#strategy')
-const restartBtn = document.querySelector('#restart')
+const boxes = document.querySelectorAll(".box");
+const text = document.querySelector("#heading");
+const strategy = document.querySelector("#strategy");
+const restartBtn = document.querySelector("#restart");
+
+let spaces = [];
+
+let player1 = "X";
+let player2 = "O";
+let currentPlayer = player1;
 
 const drawBoard = () => {
-    boxes.forEach((box, i) => {
-        let styleString = ''
-        if (i<3) {
-            styleString += 'border-bottom: 3px solid var(--text)' 
-        }
-        if (i%3===0) {
-            styleString += 'border-right: 3px solid var(--text)'
-        }
-        if (i%3 ===2) {
-            styleString += 'border-left: 3px solid var(--text)'
-        }
-        if (i>5) {
-            styleString += 'border-top: 3px solid var(--text)'
+  boxes.forEach((box, i) => {
+    let styleString = "";
+    if (i < 3) {
+      styleString += "border-bottom: 3px solid var(--text)";
+    }
+    if (i % 3 === 0) {
+      styleString += "border-right: 3px solid var(--text)";
+    }
+    if (i % 3 === 2) {
+      styleString += "border-left: 3px solid var(--text)";
+    }
+    if (i > 5) {
+      styleString += "border-top: 3px solid var(--text)";
+    }
+    box.style = styleString;
+    box.addEventListener("click", boxClicked);
+  });
+};
 
-        }
-        box.style = styleString
-    })
-}
+const boxClicked = (e) => {
+  const id = e.target.id;
+  if (!spaces[id]) {
+    spaces[id] = currentPlayer;
+    e.target.innerText = currentPlayer;
+  }
+  if (playerWon()) {
+    text.innerText = `${currentPlayer} has won!`;
+    restart();
+    return;
+  }
+  if (playerDraw()) {
+    text.innerText = "It's a draw!";
+  }
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+};
+
+restartBtn.addEventListener("click", restart);
+restart();
+drawBoard();
