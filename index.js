980be94c61ac8ddc56e73e4cdfaf640430c1cc4a
Emma Craig <emma.craig@getflowbox.com>
2024-03-14 12:02:31 +0100

    add player status text & game function

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const boxes = document.querySelectorAll(".box");
 const text = document.querySelector("#heading");
 const strategy = document.querySelector("#strategy");
 const restartBtn = document.querySelector("#restart");
+const playerOne = document.querySelector("#player1");
+const playerTwo = document.querySelector("#player2");
 
 let spaces = [];
 
-let player1 = "X";
-let player2 = "O";
-let currentPlayer = player1;
+let players = {
+  player1: "X",
+  player2: "O",
+};
+let currentPlayer = players.player1;
+console.log(currentPlayer);
+playerOne.classList.add("active");
 
 const drawBoard = () => {
   boxes.forEach((box, i) => {
@@ -28,7 +34,7 @@ const drawBoard = () => {
     if (i > 5) {
       box.className += " top-lines";
     }
-
+    currentPlayer = player1;
     box.addEventListener("click", boxClicked);
   });
 };
@@ -38,20 +44,105 @@ const boxClicked = (e) => {
   if (!spaces[id]) {
     spaces[id] = currentPlayer;
     e.target.innerText = currentPlayer;
+
+    if (playerWon()) {
+      text.innerText = `${currentPlayer} has won!`;
+      restart();
+      return;
+    }
+    if (playerDraw()) {
+      text.innerText = "It's a draw!";
+    }
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    updatePlayerLabels();
   }
-  if (playerWon()) {
-    text.innerText = `${currentPlayer} has won!`;
-    restart();
-    return;
-  }
-  if (playerDraw()) {
-    text.innerText = "It's a draw!";
-  }
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
 };
 
-const restart = () => {};
+const playerWon = () => {
+  if (spaces[0] === currentPlayer) {
+    if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+      strategy.innerText = `${currentPlayer} wins!`;
 
+      return true;
+    }
+    if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+      strategy.innerText = `${currentPlayer} wins!`;
+
+      return true;
+    }
+    if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+      strategy.innerText = `${currentPlayer} wins!`;
+
+      return true;
+    }
+  }
+  if (spaces[8] === currentPlayer) {
+    if (spaces[7] === currentPlayer && spaces[6] === currentPlayer) {
+      strategy.innerText = `${currentPlayer} wins!`;
+
+      return true;
+    }
+    if (spaces[5] === currentPlayer && spaces[2] === currentPlayer) {
+      strategy.innerText = `${currentPlayer} wins!`;
+
+      return true;
+    }
+  }
+  if (spaces[4] === currentPlayer) {
+    if (spaces[3] === currentPlayer && spaces[2] === currentPlayer) {
+      strategy.innerText = `${currentPlayer} wins!`;
+
+      return true;
+    }
+    if (spaces[1] === currentPlayer && spaces[7] === currentPlayer) {
+      strategy.innerText = `${currentPlayer} wins!`;
+
+      return true;
+    }
+    if (spaces[2] === currentPlayer && spaces[6] === currentPlayer) {
+      strategy.innerText = `${currentPlayer} wins!`;
+
+      return true;
+    }
+  }
+};
+
+const playerDraw = () => {
+  let count = 0;
+  spaces.forEach((box, i) => {
+    if (spaces[i] !== null) count++;
+  });
+  if (count === 9) {
+    text.innerText = "It's a draw!";
+    restart();
+  }
+};
+
+const updateStatus = (message) => (strategy.innerText = message);
+
+const restart = () => {
+  playerOne.classList.add("active");
+
+  setTimeout(() => {
+    spaces.forEach((space, i) => {
+      spaces[i] = null;
+    });
+    boxes.forEach((box) => {
+      box.innerText = "";
+    });
+    text.innerText = `Play`;
+    strategy.innerText = ``;
+  }, 1000);
+};
+
+const updatePlayerLabels = () => {
+  playerOne.classList.toggle("active");
+  playerTwo.classList.toggle("active");
+
+  currentPlayer === player1
+    ? updateStatus("Player 1's turn (X)")
+    : updateStatus("Player 2's turn (O)");
+};
 
 restartBtn.addEventListener("click", restart);
 restart();
